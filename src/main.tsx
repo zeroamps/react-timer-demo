710eeea5ff2a5a06c72914a489242294a6354945
Jozef Chovan <jozef.chovan@gmail.com>
2023-08-25 16:48:15 +0200

    Add Bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
+import Button from 'react-bootstrap/Button';
+import './styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <>
+      <h1 className="display-1">Hello, World!</h1>
+      <Button>Click ok me!</Button>
+    </>
   </React.StrictMode>
 );
