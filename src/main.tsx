0497b82b9c8f743dcb0f5daa2d51d2978cb08991
Jozef Chovan <jozef.chovan@gmail.com>
2023-09-10 12:16:07 +0200

    Update styling

--- src/main.tsx
+++ src/main.tsx
@@ -13,10 +13,35 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       </Navbar>
 
       <Container>
-        <div className="bg-yellow-100 mt-3 p-3 border border-1 border-yellow-300 rounded-3 text-center">
+        <div className="bg-yellow-100 mt-3 p-3 border border-yellow-300 rounded-3 text-center">
           <Button className="w-md-50 w-100" variant="success" size="lg">
             Create a new timer
           </Button>
+        </div>
+
+        <div className="bg-100 p-3 mt-3 border rounded-3">
+          <div className="fw-bold text-center text-uppercase">Birthday</div>
+          <div className="d-flex gap-3 align-items-center justify-content-center">
+            <div className="p-1 p-md-3 text-center">
+              <div className="fs-3 fs-md-1">46</div>
+              <div className="fs-9 fs-md-8">Days</div>
+            </div>
+            <div className="fs-1">:</div>
+            <div className="p-1 p-md-3 text-center">
+              <div className="fs-3 fs-md-1">12</div>
+              <div className="fs-9 fs-md-8">Hours</div>
+            </div>
+            <div className="fs-1">:</div>
+            <div className="p-1 p-md-3 text-center">
+              <div className="fs-3 fs-md-1">35</div>
+              <div className="fs-9 fs-md-8">Minutes</div>
+            </div>
+            <div className="fs-1">:</div>
+            <div className="p-1 p-md-3 text-center">
+              <div className="fs-3 fs-md-1">12</div>
+              <div className="fs-9 fs-md-8">Seconds</div>
+            </div>
+          </div>
         </div>
       </Container>
     </>
