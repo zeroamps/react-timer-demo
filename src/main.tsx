1cd2acaa96931b2af9a9d63d8b0f330334552c1c
Jozef Chovan <jozef.chovan@gmail.com>
2023-09-10 09:00:50 +0200

    Update styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Button from 'react-bootstrap/Button';
+import { Button, Container, Navbar } from 'react-bootstrap';
 import './styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <>
-      <h1 className="display-1">Hello, World!</h1>
-      <Button>Click ok me!</Button>
+      <Navbar className="bg-primary">
+        <Container>
+          <Navbar.Brand className="text-uppercase text-white mx-auto">React Timer Demo</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Container>
+        <div className="bg-yellow-100 mt-3 p-3 border border-1 border-yellow-300 rounded-3 text-center">
+          <Button className="w-md-50 w-100" variant="success" size="lg">
+            Create a new timer
+          </Button>
+        </div>
+      </Container>
     </>
   </React.StrictMode>
 );
