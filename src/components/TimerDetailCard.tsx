32b77b4ba9aea135c747be723ba0e76751e52d09
Jozef Chovan <jozef.chovan@gmail.com>
2023-09-13 07:32:23 +0200

    Implement finished

--- src/components/TimerDetailCard.tsx
+++ src/components/TimerDetailCard.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import classNames from 'classnames';
 import { Button, Card } from 'react-bootstrap';
 import { Timer } from '../domains';
 import { TimerEditDialog } from './TimerEditDialog';
@@ -11,10 +12,13 @@ type Props = {
 export function TimerDetailCard({ timer }: Props) {
   const [showEditDialog, setShowEditDialog] = useState(false);
   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const finished = timer.target <= new Date();
 
   return (
-    <Card className="bg-100 mt-3 border-300">
-      <Card.Header className="border-bottom border-300">{timer.name}</Card.Header>
+    <Card className={classNames('mt-3', finished ? 'bg-green-100 border-green-300' : 'bg-100 border-300')}>
+      <Card.Header className={classNames('border-bottom', finished ? 'border-green-300' : 'border-300')}>
+        {timer.name}
+      </Card.Header>
       <Card.Body>
         <div className="d-flex gap-3 align-items-center justify-content-center">
           <div className="p-1 p-md-3 text-center">
@@ -38,7 +42,11 @@ export function TimerDetailCard({ timer }: Props) {
           </div>
         </div>
       </Card.Body>
-      <Card.Footer className="d-flex gap-1 justify-content-end p-1 border-top border-300">
+      <Card.Footer
+        className={classNames(
+          'd-flex gap-1 justify-content-end p-1 border-top',
+          finished ? 'border-green-300' : 'border-300'
+        )}>
         <Button className="fs-8 px-3 p-0" variant="success" size="sm" onClick={() => setShowEditDialog(true)}>
           Edit
         </Button>
@@ -50,41 +58,4 @@ export function TimerDetailCard({ timer }: Props) {
       <TimerDeleteDialog show={showDeleteDialog} onClose={() => setShowDeleteDialog(false)} />
     </Card>
   );
-}
-
-{
-  /* <Card className="bg-green-100 mt-3 border border-green-300">
-  <Card.Header className="border-bottom border-green-300">Birthday</Card.Header>
-  <Card.Body>
-    <div className="d-flex gap-3 align-items-center justify-content-center">
-      <div className="p-1 p-md-3 text-center">
-        <div className="fs-3 fs-md-1">46</div>
-        <div className="fs-9 fs-md-8">Days</div>
-      </div>
-      <div className="fs-1">:</div>
-      <div className="p-1 p-md-3 text-center">
-        <div className="fs-3 fs-md-1">12</div>
-        <div className="fs-9 fs-md-8">Hours</div>
-      </div>
-      <div className="fs-1">:</div>
-      <div className="p-1 p-md-3 text-center">
-        <div className="fs-3 fs-md-1">35</div>
-        <div className="fs-9 fs-md-8">Minutes</div>
-      </div>
-      <div className="fs-1">:</div>
-      <div className="p-1 p-md-3 text-center">
-        <div className="fs-3 fs-md-1">12</div>
-        <div className="fs-9 fs-md-8">Seconds</div>
-      </div>
-    </div>
-  </Card.Body>
-  <Card.Footer className="d-flex gap-1 justify-content-end p-1 border-top border-green-300">
-    <Button className="fs-8 px-3 p-0" variant="success" size="sm">
-      Edit
-    </Button>
-    <Button className="fs-8 px-3 p-0" variant="danger" size="sm">
-      Delete
-    </Button>
-  </Card.Footer>
-</Card>; */
 }
--- src/components/TimerList.tsx
+++ src/components/TimerList.tsx
@@ -4,7 +4,7 @@ import { Timer } from '../domains';
 
 const timers: Timer[] = [
   { id: '1', name: 'Birthday', target: new Date(2024, 1, 1) },
-  { id: '2', name: 'Birthday', target: new Date(2024, 1, 1) },
+  { id: '2', name: 'Birthday', target: new Date(2023, 1, 1) },
   { id: '3', name: 'Birthday', target: new Date(2024, 1, 1) }
 ];
 
